4cfadc0761e521441eb854322ecd5c135c740896
neilpeterson <neilpeterson@outlook.com>
2021-08-30 15:50:12 -0700

    script update

--- config/hyperv.ps1
+++ config/hyperv.ps1
@@ -1,7 +1,6 @@
 configuration windowsfeatures {
 
     Import-DscResource -ModuleName PsDesiredStateConfiguration
-    Import-DscResource –ModuleName xHyper-V
 
     node localhost {
 
@@ -9,19 +8,6 @@ configuration windowsfeatures {
             Ensure = "Present"
             Name = "Hyper-V"
             IncludeAllSubFeature = $true
-        }
-
-        File VMsDirectory {
-            Ensure = 'Present'
-            Type = 'Directory'
-            DestinationPath = "$($env:SystemDrive)\VMs"
-        }     
-        
-        xVMSwitch LabSwitch {
-            DependsOn = '[WindowsFeature]Hyper-V'        
-            Name = 'LabSwitch'
-            Ensure = 'Present'
-            Type = 'Internal'    
         }
     }
 }
