efc6e1bfda202dc119d00696014d7b7005da4263
neilpeterson <neilpeterson@outlook.com>
2021-08-31 11:48:27 -0700

    script update

--- config/hyperv.ps1
+++ config/hyperv.ps1
@@ -24,8 +24,8 @@ configuration windowsfeatures {
                 Test-Path "c:\test.txt" 
             }
             SetScript = {
-                # cmd.exe /C "cmdkey /add:`"nepetersosios.file.core.windows.net`" /user:`"localhost\nepetersosios`" /pass:$Pass"
-                # New-PSDrive -Name Z -PSProvider FileSystem -Root "\\nepetersosios.file.core.windows.net\windows-os-iso" -Persist
+                cmd.exe /C "cmdkey /add:`"nepetersosios.file.core.windows.net`" /user:`"localhost\nepetersosios`" /pass:$Pass"
+                New-PSDrive -Name Z -PSProvider FileSystem -Root "\\nepetersosios.file.core.windows.net\windows-os-iso" -Persist
                 New-Item c:\test.txt
             }
         }
