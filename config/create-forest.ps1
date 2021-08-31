4dd5dd1040e41928adb8508962f979373254a3c9
neilpeterson <neilpeterson@outlook.com>
2021-08-31 10:05:21 -0700

    script update

---
+++ config/create-forest.ps1
@@ -0,0 +1,103 @@
+configuration CreateForest 
+{ 
+   param 
+   ( 
+        [Parameter(Mandatory)]
+        [String]$DomainName,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$Admincreds,
+
+        [Int]$RetryCount=20,
+        [Int]$RetryIntervalSec=30
+    ) 
+    
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
+    Import-DscResource -ModuleName xActiveDirectory
+    Import-DscResource -ModuleName xNetworking
+    Import-DscResource -ModuleName xPendingReboot
+    Import-DSCResource -ModuleName StorageDsc
+    
+    [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
+    $Interface=Get-NetAdapter | Where Name -Like "Ethernet*" | Select-Object -First 1
+
+    Node localhost
+    {
+        LocalConfigurationManager 
+        {
+            ActionAfterReboot = 'ContinueConfiguration'            
+            ConfigurationMode = 'ApplyOnly'
+            RebootNodeIfNeeded = $true
+        }
+
+        WaitForDisk Disk2
+        {
+            DiskId = 2
+            RetryIntervalSec = 60
+            RetryCount = 20
+        }
+        
+        Disk FVolume
+        {
+            DiskId = 2
+            DriveLetter = 'F'
+            FSLabel = 'Data'
+            FSFormat = 'NTFS'
+            DependsOn = '[WaitForDisk]Disk2'
+        }   
+
+	    WindowsFeature DNS 
+        { 
+            Ensure = "Present" 
+            Name = "DNS"		
+        }
+
+	    WindowsFeature DnsTools
+	    {
+	        Ensure = "Present"
+            Name = "RSAT-DNS-Server"
+            DependsOn = "[WindowsFeature]DNS"
+	    }
+
+        xDnsServerAddress DnsServerAddress 
+        { 
+            Address        = '127.0.0.1' 
+            InterfaceAlias = $Interface.Name
+            AddressFamily  = 'IPv4'
+	        DependsOn = "[WindowsFeature]DNS"
+        }
+
+        WindowsFeature ADDSInstall 
+        { 
+            Ensure = "Present" 
+            Name = "AD-Domain-Services"
+	        DependsOn="[WindowsFeature]DNS" 
+        } 
+
+        WindowsFeature ADDSTools
+        {
+            Ensure = "Present"
+            Name = "RSAT-ADDS-Tools"
+            DependsOn = "[WindowsFeature]ADDSInstall"
+        }
+
+        WindowsFeature ADAdminCenter
+        {
+            Ensure = "Present"
+            Name = "RSAT-AD-AdminCenter"
+            DependsOn = "[WindowsFeature]ADDSInstall"
+        }
+         
+        xADDomain FirstDS 
+        {
+            DomainName = $DomainName
+            DomainAdministratorCredential = $DomainCreds
+            SafemodeAdministratorPassword = $DomainCreds
+            DatabasePath = "F:\NTDS"
+            LogPath = "F:\NTDS"
+            SysvolPath = "F:\SYSVOL"
+	        DependsOn = @("[WindowsFeature]ADDSInstall")
+        } 
+
+   }
+} 
--- config/hyperv.ps1
+++ config/hyperv.ps1
@@ -15,13 +15,13 @@ configuration windowsfeatures {
             IncludeAllSubFeature = $true
         }
 
-        Script ScriptExample
-        {
-            SetScript = {
-                # cmd.exe /C "cmdkey /add:`"nepetersosios.file.core.windows.net`" /user:`"localhost\nepetersosios`" /pass:$Pass"
-                # New-PSDrive -Name Z -PSProvider FileSystem -Root "\\nepetersosios.file.core.windows.net\windows-os-iso" -Persist
-                New-Item c:\test.txt
-            }
-        }
+        # Script ScriptExample
+        # {
+        #     SetScript = {
+        #         # cmd.exe /C "cmdkey /add:`"nepetersosios.file.core.windows.net`" /user:`"localhost\nepetersosios`" /pass:$Pass"
+        #         # New-PSDrive -Name Z -PSProvider FileSystem -Root "\\nepetersosios.file.core.windows.net\windows-os-iso" -Persist
+        #         New-Item c:\test.txt
+        #     }
+        # }
     }
 }
