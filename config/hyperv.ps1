d63e18909a3eaef018554753f9ce0327864d605c
neilpeterson <neilpeterson@outlook.com>
2021-08-31 22:24:50 -0700

    script update

--- config/hyperv.ps1
+++ config/hyperv.ps1
@@ -9,41 +9,17 @@ configuration windowsfeatures {
 
     node localhost {
 
-        WindowsFeature WebServer {
+        WindowsFeature Hyper-V {
             Ensure = "Present"
             Name = "Hyper-V"
             IncludeAllSubFeature = $true
         }
 
         xVMSwitch LabSwitch {
-
             DependsOn = '[WindowsFeature]Hyper-V'
             Name = 'LabSwitch'
                  Ensure = 'Present'
             Type = 'Internal'
         }
-
-    #     Script ScriptExample
-    #     {
-    #         GetScript = { 
-    #             $results = Get-Item "c:\test.txt"
-    #         }
-    #         TestScript= { 
-    #             Test-Path "c:\test.txt" 
-    #         }
-    #         SetScript = {
-    #             # cmd.exe /C "cmdkey /add:`"nepetersosios.file.core.windows.net`" /user:`"localhost\nepetersosios`" /pass:`"uCw54u3owm0Yq0RGxoG0LEx/hB2WjoipIfcmgzxm5iey1NdhXGSvchXP3SQ9XbJioaE3xpiWeqFmY3KhEp80lA==`""
-    #             # New-PSDrive -Name Z -PSProvider FileSystem -Root "\\nepetersosios.file.core.windows.net\windows-os-iso" -Persist
-    #             # New-Item c:\test.txt
-
-    #             $secpasswd = ConvertTo-SecureString "Monkeyskip76" -AsPlainText -Force
-    #             $mycreds = New-Object System.Management.Automation.PSCredential ("Administrator", $secpasswd)
-    #             $output = Invoke-Command -ScriptBlock { 
-    #                 cmd.exe /C "cmdkey /add:`"nepetersosios.file.core.windows.net`" /user:`"localhost\nepetersosios`" /pass:`"uCw54u3owm0Yq0RGxoG0LEx/hB2WjoipIfcmgzxm5iey1NdhXGSvchXP3SQ9XbJioaE3xpiWeqFmY3KhEp80lA==`""
-    #                 New-PSDrive -Name Z -PSProvider FileSystem -Root "\\nepetersosios.file.core.windows.net\windows-os-iso" -Persist
-    #                 New-Item c:\test.txt
-    #              } -ComputerName localhost -Credential $mycreds -Verbose
-    #         }
-    #     }
-    # }
+    }
 }
