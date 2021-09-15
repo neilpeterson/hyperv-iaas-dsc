ef557379accb695dbfc8dff48b68f3c8ccac544a
Neil Peterson <neilpeterson@outlook.com>
2021-09-14 21:01:27 -0700

    script update

--- scritps/upload-compule-config.ps1
+++ scritps/upload-compule-config.ps1
@@ -1,13 +1,16 @@
 [CmdletBinding()]
 param (
     [Parameter()]
-    [string]$automationAccountName = "sdmszrkthtqfo",
+    [string]$automationAccountName = "test-automation",
 
     [Parameter()]
-    [string]$resourceGroupName = "automation-central-001",
+    [string]$resourceGroupName = "test-automation",
 
     [Parameter()]
     [string]$path = "../config/addc.ps1"
 )
 
 Import-AzAutomationDscConfiguration -AutomationAccountName $automationAccountName -ResourceGroupName $resourceGroupName -SourcePath $path -Force -Published
+
+$Params = @{"DomainName"="contoso.com"}
+Start-AzAutomationDscCompilationJob -ConfigurationName "addc" -Parameters $Params -AutomationAccountName $automationAccountName -ResourceGroupName $resourceGroupName
