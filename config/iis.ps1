da4f6a6d5d5d91c336dbf4e2bb0b7a312d207315
neilpeterson <neilpeterson@outlook.com>
2021-09-14 17:27:17 -0700

    script

---
+++ config/iis.ps1
@@ -0,0 +1,13 @@
+Configuration iis
+{
+    Import-DscResource -ModuleName 'PSDesiredStateConfiguration'
+
+    node ('localhost')
+    {
+        WindowsFeature IIS
+        {
+            Ensure = 'Present'
+            Name   = 'Web-Server'
+        }
+    }
+}
