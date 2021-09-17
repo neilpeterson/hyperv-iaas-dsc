1f4dd631a2b9f4908f5c44f08973c3a769e49ac0
neilpeterson <neilpeterson@outlook.com>
2021-09-17 14:20:55 -0700

    scritp update

--- scritps/upload-compule-config.ps1
+++ scritps/upload-compule-config.ps1
@@ -4,13 +4,13 @@ param (
     [string]$automationAccountName = "test-automation-account",
 
     [Parameter()]
-    [string]$resourceGroupName = "test-automation-account",
-
-    [Parameter()]
-    [string]$path = "../config/hyperv.ps1"
+    [string]$resourceGroupName = "test-automation-account"
 )
 
-Import-AzAutomationDscConfiguration -AutomationAccountName $automationAccountName -ResourceGroupName $resourceGroupName -SourcePath $path -Force -Published
+# Import-AzAutomationDscConfiguration -AutomationAccountName $automationAccountName -ResourceGroupName $resourceGroupName -SourcePath "../config/hyperv.ps1" -Force -Published
+# Import-AzAutomationDscConfiguration -AutomationAccountName $automationAccountName -ResourceGroupName $resourceGroupName -SourcePath "../config/addc.ps1" -Force -Published
+Import-AzAutomationDscConfiguration -AutomationAccountName $automationAccountName -ResourceGroupName $resourceGroupName -SourcePath "../config/rodc.ps1" -Force -Published
+# Import-AzAutomationDscConfiguration -AutomationAccountName $automationAccountName -ResourceGroupName $resourceGroupName -SourcePath "../config/iis.ps1" -Force -Published
 
-$Params = @{"DomainName"="contoso.com";"ComputerName"="hyperv-vm";"DNSAddress"="10.0.2.4"}
-Start-AzAutomationDscCompilationJob -ConfigurationName "hyperv" -Parameters $Params -AutomationAccountName $automationAccountName -ResourceGroupName $resourceGroupName
+# $Params = @{"DomainName"="contoso.com";"ComputerName"="hyperv-vm";"DNSAddress"="10.0.2.4"}
+# Start-AzAutomationDscCompilationJob -ConfigurationName "hyperv" -Parameters $Params -AutomationAccountName $automationAccountName -ResourceGroupName $resourceGroupName
