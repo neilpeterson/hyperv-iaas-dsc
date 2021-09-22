d99ff0ce6e4e3b4e20ce4c151ab1333ef4dc6d0b
neilpeterson <neilpeterson@outlook.com>
2021-09-22 09:53:49 -0700

    scritp update

--- scritps/upload-compule-config.ps1
+++ scritps/upload-compule-config.ps1
@@ -7,10 +7,12 @@ param (
     [string]$resourceGroupName = "test-automation-account"
 )
 
-# Import-AzAutomationDscConfiguration -AutomationAccountName $automationAccountName -ResourceGroupName $resourceGroupName -SourcePath "../config/hyperv.ps1" -Force -Published
-# Import-AzAutomationDscConfiguration -AutomationAccountName $automationAccountName -ResourceGroupName $resourceGroupName -SourcePath "../config/addc.ps1" -Force -Published
+Import-AzAutomationDscConfiguration -AutomationAccountName $automationAccountName -ResourceGroupName $resourceGroupName -SourcePath "../config/addc.ps1" -Force -Published
+Import-AzAutomationDscConfiguration -AutomationAccountName $automationAccountName -ResourceGroupName $resourceGroupName -SourcePath "../config/hyperv.ps1" -Force -Published
+Import-AzAutomationDscConfiguration -AutomationAccountName $automationAccountName -ResourceGroupName $resourceGroupName -SourcePath "../config/iis.ps1" -Force -Published
+Import-AzAutomationDscConfiguration -AutomationAccountName $automationAccountName -ResourceGroupName $resourceGroupName -SourcePath "../config/member.ps1" -Force -Published
 Import-AzAutomationDscConfiguration -AutomationAccountName $automationAccountName -ResourceGroupName $resourceGroupName -SourcePath "../config/rodc.ps1" -Force -Published
-# Import-AzAutomationDscConfiguration -AutomationAccountName $automationAccountName -ResourceGroupName $resourceGroupName -SourcePath "../config/iis.ps1" -Force -Published
 
-# $Params = @{"DomainName"="contoso.com";"ComputerName"="hyperv-vm";"DNSAddress"="10.0.2.4"}
-# Start-AzAutomationDscCompilationJob -ConfigurationName "hyperv" -Parameters $Params -AutomationAccountName $automationAccountName -ResourceGroupName $resourceGroupName
+# # $Params = @{"DomainName"="contoso.com";"ComputerName"="hyperv-vm";"DNSAddress"="10.0.2.4"}
+# $Params = @{"DomainName"="contoso.com"}
+# Start-AzAutomationDscCompilationJob -ConfigurationName "addc" -Parameters $Params -AutomationAccountName $automationAccountName -ResourceGroupName $resourceGroupName
